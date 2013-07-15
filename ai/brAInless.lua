2a3a256306470f340e8d593ecdf39d81cea9124c
Maximilian Stauss <max.stauss@gmail.com>
2013-07-15 18:37:50 +0200

    added brAInless as a random AI and a boilerplate

--- ai/brAInless.lua
+++ ai/brAInless.lua
@@ -1,153 +1,99 @@
--- require Snippets
-require('./_snippets/_all.lua')
---
-
+require('_snippets/_all.lua')
 
 rememberPassengers = {}
 rememberMap = nil
 
-function ai.init()
-  print("Hi, I'm TestAI1.")
-end
-
-function printTable(table, lvl)
-  if not type(table) == "table" then
-    print("not a table!")
-    return
-  end
-  lvl = lvl or 0
-  for k, v in pairs(table) do
-    if type(v) == "table" then
-      printTable(table, lvl + 1)
-    else
-      str = ""
-      for i = 1,lvl do
-        str = str .. "\t"
-      end
-      print(str, k, v)
-    end
-  end
-end
-
-
+-- Wird aufgerufen, wenn das Spiel startet
 function ai.init(map, money)
   print("Initialized! Hello World!")
   print("I got " .. money .. " credits to spend!!")
   print("map size:", map.height, map.width)
   rememberMap = map
+
+
   
-  x = random(map.width)
-  y = random(map.height)
-  while money >= 25 do    -- 25c is cost of one train
-    buyTrain(x, y)
+  while money >= 25 do    -- Ein Zug kostet 25 Credits
+    buyTrain(random(map.width), random(map.height))
     money = money - 25
   end
 end
 
-
-function printTable(table, lvl)
-  lvl = lvl or 0
-  if lvl > 2 then return end
-  for k, v in pairs(table) do
-    if type(v) == "table" then
-      --printTable(table, lvl + 1)
-    else
-      str = ""
-      for i = 1,lvl do
-        str = str .. "\t"
-      end
-      print(str, k, v)
-    end
-  end
-end
-
-function chooseSmart(train, possibleDirections)
-  if train.passenger then
-    print("train.passenger destination:", rememberPassengers[train.passenger].destX, rememberPassengers[train.passenger].destY)
-    printTable(rememberPassengers)
-    print("train.pos:", train.x, train.y)
-    
-    if possibleDirections["N"] and rememberPassengers[train.passenger].destY+1 < train.y and random() < .7 then
-      print("I think North is best")
-      return "N"
-    end
-    if possibleDirections["S"] and rememberPassengers[train.passenger].destY-1 > train.y and random() < .7 then
-      print("I think South is best")
-      return "S"
-    end
-    if possibleDirections["E"] and rememberPassengers[train.passenger].destX+1 > train.x and random() < .7 then
-      print("I think East is best")
-      return "E"
-    end
-    if possibleDirections["W"] and rememberPassengers[train.passenger].destX-1 < train.x and random() < .7 then
-      print("I think West is best")
-      return "W"
-    end
-  end
-end
-
+-- Wird aufgerufen, wenn genug geld für einen neuen Zug da ist
 function ai.enoughMoney(money)
   x = random(rememberMap.width)
   y = random(rememberMap.height)
-  while money >= 25 do    -- 25c is cost of one train
-    buyTrain(x, y)
-    money = money - 25
-  end
+  buyTrain(x, y)
 end
 
-function chooseRandom(train, possibleDirections)
-  tbl = {}
-  if possibleDirections["N"] then
-    tbl[#tbl+1] = "N"
-  end
-  if possibleDirections["S"] then
-    tbl[#tbl+1] = "S"
-  end
-  if possibleDirections["E"] then
-    tbl[#tbl+1] = "E"
-  end
-  if possibleDirections["W"] then
-    tbl[#tbl+1] = "W"
-  end
-  return tbl[random(#tbl)]
-end
-
+-- Wird aufgerufen, wenn eine Richtungswahl ansteht
+-- train [table]
+-- possibleDirections [table]
 function ai.chooseDirection(train, possibleDirections)
-  print("I have: " .. getMoney())
-  -- choose a direction that makes sense.
-  -- otherwise, return a random direction:
-  return chooseSmart(train, possibleDirections) or chooseRandom(train, possibleDirections)
+  return chooseBrainless(train, possibleDirections)
 end
 
+-- Wird aufgerufen, wenn der Zug blockiert wird
+-- (Beispielsweise durch einen anderen Zug)
 function ai.blocked(train, possibleDirections, lastDirection)
-  return chooseSmart(train, possibleDirections) or chooseRandom(train, possibleDirections)
+  return chooseRandom(train, possibleDirections)
 end
 
+-- Wird aufgerufen, wenn der Zug an einem Passagier vorbeikommt
 function ai.foundPassengers(train, passengers)
-  if train.passenger and train.passenger.name:find("VIP") then return nil end
-  
-  for k, p in pairs(passengers) do
-    print(p)
-    if p.name:find("VIP") then
-      print("found VIP!")
-      if train.passenger then
-        dropPassenger(train)
-      end
-      return p
-    end
-  end
+  if train.passenger then return nil end
   return passengers[1]
 end
 
+-- Wird aufgerufen, wenn der Zug am Ziel des Passagiers ankommt
 function ai.foundDestination(train)
   print("get off!")
   dropPassenger(train)
+  
+  print("Money: " .. getMoney())
 end
 
-function ai.passengerBoarded(train, passenger)
-  print("I see you! " .. train.ID .. " " .. passenger)
-end
-
+-- Wird aufgerufen, wenn ein neuer Fahrgast erscheint
 function ai.newPassenger(name, x, y, destX, destY)
+  print("Neuer Fahrgast: " .. name)
   rememberPassengers[name] = {x=x,y=y,destX=destX,destY=destY}
+end
+
+-- ------------
+-- LOCAL HELPER
+-- ------------
+
+-- Wenn ein Passagier an Board ist wird versucht diesen ans Ziel zu bringen
+function chooseBrainless(train, possibleDirections)
+  if train.passenger then 
+    print("train.passenger destination:", train.passenger.destX, train.passenger.destY)
+    print("train.pos:", train.x, train.y)
+    
+    if possibleDirections["N"] and train.passenger.destY+1 < train.y and random() < .7 then
+      print("Auf nach Norden!")
+      return "N"
+    end
+    if possibleDirections["S"] and train.passenger.destY-1 > train.y and random() < .7 then
+      print("Auf nach Süden")
+      return "S"
+    end
+    if possibleDirections["E"] and train.passenger.destX+1 > train.x and random() < .7 then
+      print("Auf nach Osten!")
+      return "E"
+    end
+    if possibleDirections["W"] and train.passenger.destY-1 < train.x and random() < .7 then
+      print("Auf nach Westen")
+      return "W"
+    end
+  else -- kein Passagier
+    return chooseRandom(train, possibleDirections)
+  end
+end
+
+-- wählt zufällig aus den möglichen Richtungen eine aus
+function chooseRandom(train, possibleDirections)
+  tbl = {}
+  for dir,bool in pairs(possibleDirections) do 
+    tbl[#tbl+1] = dir
+  end
+  return tbl[random(#tbl)]
 end
