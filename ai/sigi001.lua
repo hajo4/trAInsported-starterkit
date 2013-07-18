2c27931cbba28615916dca63d837bf9da2f37f99
Sigi <s.gessulat@gmail.com>
2013-07-18 16:28:03 +0200

    fixed bug, when seeing passengers

--- ai/sigi001.lua
+++ ai/sigi001.lua
@@ -19,6 +19,15 @@ end
 
 -- Tutorial 4: Close is good!
 railMap = {}
+passengerMap = nil
+
+function loc2String( loc )
+    return loc.x.." "..loc.y.." "..loc.dir
+end
+
+function makeLoc( x, y, dir)
+    return { ["x"]=x, ["y"]=y, ["dir"]=dir }
+end
 
 function dir2String( map, loc )
     x = loc.x
@@ -45,7 +54,13 @@ function ai.init(map, money, maximumTrains)
     railMap = map2railMap(map)
     buyTrain(random(map.width), random(map.height))
 
-
+    spawned_passengers = {}
+    for i=1, railMap.width do
+        spawned_passengers[i] = {}
+        for j=1, railMap.height do
+            spawned_passengers[i][j]= nil
+        end
+    end
 
     loc = { ["x"]=4, ["y"]=2, ["dir"]="S" }
     target = { ["x"]=10, ["y"]=5}
@@ -65,15 +80,49 @@ function distance(x1, y1, x2, y2)
 end
 
 function ai.chooseDirection(train, directions)
+    local dir = nil
+    if train.passenger then
+        -- find path
+        local trainLoc = makeLoc(train.x, train.y, train.dir)
+        local crossingsLoc = goDir( railMap, trainLoc, train.dir )
+        print("Train Loc "..loc2String(crossingsLoc))
+        print("Crossing Loc "..loc2String(crossingsLoc))
+        print( passengerMap[crossingsLoc.x][crossingsLoc.y] )
+        dir = passengerMap[crossingsLoc.x][crossingsLoc.y]
+    else
+        -- RANDOM ATTACK!!
+        print('go random')
+        local tbl = {}
+        for dir,bool in pairs(directions) do
+            tbl[#tbl+1] = dir
+        end
+   
+        dir = tbl[random(#tbl)]
+    end
+    return dir
 
 end
 
 function ai.newPassenger(name, x, y, destX, destY, vipTime)
+
 end
 
 
-
 function ai.foundPassengers( train, passengers )
+    if train.passenger then
+        print('seeing a passenger')
+    else
+        local p = passengers[1] -- this could be more clever
+        local location = {["x"]=train.x,["y"]=train.y,["dir"]=train.dir }
+        local target = { ["x"]=p.destX, ["y"]=p.destY, }
+        local path = searchPath( 0, railMap, location, target )
+        passengerMap = railMap
+        for i=1, #path do 
+            print(path[i].x.." "..path[i].y)
+            passengerMap[ path[i].x ][ path[i].y ] = path[i].dir 
+        end
+        print('getting passenger')
+    end
     return passengers[1]
 end
 
